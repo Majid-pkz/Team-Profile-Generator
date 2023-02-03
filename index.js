f259065efefad08b0924c39c5cb0a38b2960b959
Majid-pkz <majid.computing@gmail.com>
2023-02-04 02:36:02 +1100

    starter code

---
+++ index.js
@@ -0,0 +1,9 @@
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+
+
+//const karmand = new Manager("Majid",234,"maj@g.com");
+
