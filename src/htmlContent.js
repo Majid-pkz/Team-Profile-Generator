9da63686a4c599eeb29fcb5c5e76522408817e62
Majid-pkz <majid.computing@gmail.com>
2023-04-24 08:29:54 +1000

    add mailto option

--- src/htmlContent.js
+++ src/htmlContent.js
@@ -1,9 +1,7 @@
-
-const fs= require('fs');
+const fs = require("fs");
 function generateTeam(team) {
-
-    function generateManager(manager) {
-        return `
+  function generateManager(manager) {
+    return `
            
         <div class="card m-4 " style="width: 18rem;">
             <div class="card-header text-bg-primary fs-3 p-2 ">
@@ -12,16 +10,17 @@ function generateTeam(team) {
             </div>
             <ul class="list-group list-group-flush">
               <li class="list-group-item"><span>ID:  </span>${manager.getId()} </li>
-              <li class="list-group-item"><span>Email: </span>${manager.getEmail()} </li>
-              <li class="list-group-item"><span>Office Number: </span>${manager.officeNumber} </li>
+              <li class="list-group-item"><span>Email: <a href="mailto: ${manager.getEmail()}">${manager.getEmail()}</a></li>
+              <li class="list-group-item"><span>Office Number: </span>${
+                manager.officeNumber
+              } </li>
             </ul>
          </div>    
     
-        `
-
-     }
-     function generateEngineer(engineer) {
-        return `
+        `;
+  }
+  function generateEngineer(engineer) {
+    return `
            
         <div class="card m-4 " style="width: 18rem;">
             <div class="card-header text-bg-primary fs-3 p-2 ">
@@ -30,16 +29,15 @@ function generateTeam(team) {
             </div>
             <ul class="list-group list-group-flush">
               <li class="list-group-item"><span>ID:  </span>${engineer.getId()} </li>
-              <li class="list-group-item"><span>Email: </span>${engineer.getEmail()} </li>
+              <li class="list-group-item">Email: <a href="mailto: ${engineer.getEmail()}">${engineer.getEmail()}</a>  </li>
               <li class="list-group-item"><span>Github username: </span>${engineer.getGithub()} </li>
             </ul>
          </div>    
     
-        `
-        
-     }
-     function generateIntern(intern) {
-        return `
+        `;
+  }
+  function generateIntern(intern) {
+    return `
            
         <div class="card m-4 " style="width: 18rem;">
             <div class="card-header text-bg-primary fs-3 p-2 ">
@@ -48,37 +46,33 @@ function generateTeam(team) {
             </div>
             <ul class="list-group list-group-flush">
               <li class="list-group-item"><span>ID:  </span>${intern.getId()} </li>
-              <li class="list-group-item"><span>Email: </span>${intern.getEmail()} </li>
+              <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a> </li>
               <li class="list-group-item"><span>School Name: </span>${intern.getSchool()} </li>
             </ul>
          </div>    
     
-        `
-        
-     }     
+        `;
+  }
 
-    const htmlData = []
+  const htmlData = [];
 
-    htmlData.push(team
-        .filter(e => e.getRole() === 'Manager')
-        .map(m => generateManager(m))
-        );
-        htmlData.push(team
-            .filter(e => e.getRole() === 'Engineer')
-            .map(m => generateEngineer(m))
-            );
-            htmlData.push(team
-                .filter(e => e.getRole() === 'Intern')
-                .map(m => generateIntern(m))
-                )
+  htmlData.push(
+    team.filter((e) => e.getRole() === "Manager").map((m) => generateManager(m))
+  );
+  htmlData.push(
+    team
+      .filter((e) => e.getRole() === "Engineer")
+      .map((m) => generateEngineer(m))
+  );
+  htmlData.push(
+    team.filter((e) => e.getRole() === "Intern").map((m) => generateIntern(m))
+  );
 
-       
+  return htmlData.join("");
+}
 
-        return htmlData.join("")
- }
-
-module.exports = team => {
-    return `
+module.exports = (team) => {
+  return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -91,14 +85,16 @@ module.exports = team => {
     </head>
     <body>
     <header  class="text-center text-bg-danger fs-1  p-4">My Team</header>
-    <main class=" container  row  d-flex justify-content-around">
+    <main class=" container  row  d-flex m-5">
 
-        <div class="row">
-        ${generateTeam(addedMembers)}
+        <div class="row justify-content-center">
+         
+          ${generateTeam(addedMembers)}
+         
         </div>
     </main>
         
     </body>
     </html>
-    `
-}
+    `;
+};
